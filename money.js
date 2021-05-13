be09473b737b61276f5a165a84a776aefa1c8000
diazhector98 <diazhector98@gmail.com>
2021-05-13 14:30:57 -0700

    moviendo billetes

--- money.js
+++ money.js
@@ -29,14 +29,14 @@ export const createMoney = (scene) => {
     const b1Color = 0xabffda
     const b1Materials = createMoneyMaterials(b1Texture, b1Color)
     const b1Geometry = getBoxGeometry(1, 0.15, .60);
-    const b1 = createInstance(scene, b1Geometry, b1Materials, 2.8, 1, -4)
+    const b1 = createInstance(scene, b1Geometry, b1Materials, 3.1, 0, -4)
 
     // $5
     const b2Texture = loader.load('assets/100.jpeg');
     const b2Color = 0xffe7ab
     const b2Materials = createMoneyMaterials(b2Texture, b2Color)
     const billete5Geometry = getBoxGeometry(1, 0.15, .60);
-    const billete5 = createInstance(scene, billete5Geometry, b2Materials, 2.8, 1.2, -4)
+    const billete5 = createInstance(scene, billete5Geometry, b2Materials, 3.1, -0.5, -3)
 
 
 }
