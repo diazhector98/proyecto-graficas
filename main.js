b178069662c83ff17430ed48d1b64e673415f921
Jesus Lozano <jesus.javi.lozano@hotmail.com>
2021-05-17 21:13:31 -0500

    Se agregaron las piezas de jugadores en el inicio del juego

--- main.js
+++ main.js
@@ -39,6 +39,26 @@ function main() {
     const chanceCardsGeometry = getBoxGeometry(.60, 0.15, .70);
     const chanceCards = createInstance(scene, chanceCardsGeometry, ccMaterial, 0, -0.37, -3)
 
+    //player piece 1
+    const ppMaterial = new THREE.MeshPhongMaterial({color: green})
+    const ppGeometry = getBoxGeometry(.15, .4, .15);
+    const playerPiece = createInstance(scene, ppGeometry, ppMaterial, 2, -.96, -1.9)
+
+    //player piece 2
+    const pp1Material = new THREE.MeshPhongMaterial({color: blue})
+    const pp1Geometry = getBoxGeometry(.15, .4, .15);
+    const playerPiece1 = createInstance(scene, pp1Geometry, pp1Material, 2.3, -.8, -2.15)
+    
+    //player piece 3
+    const pp2Material = new THREE.MeshPhongMaterial({color: white})
+    const pp2Geometry = getBoxGeometry(.15, .4, .15);
+    const playerPiece2 = createInstance(scene, pp2Geometry, pp2Material, 2, -.8, -2.15)
+
+    //player piece 4
+    const pp3Material = new THREE.MeshPhongMaterial({color: orange})
+    const pp3Geometry = getBoxGeometry(.15, .4, .15);
+    const playerPiece3 = createInstance(scene, pp3Geometry, pp3Material, 2.3, -.96, -1.9)
+
     // Money
     createMoney(scene)
 
